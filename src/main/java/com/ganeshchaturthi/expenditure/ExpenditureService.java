79c621fd1559a93f6c70032658b2ad03b8d4c52c
deepu <kogatamk.19.beis@acharya.ac.in>
2024-08-30 23:42:39 +0530

    11

--- src/main/java/com/ganeshchaturthi/expenditure/ExpenditureService.java
+++ src/main/java/com/ganeshchaturthi/expenditure/ExpenditureService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Service
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("https://vinakayaexpenditure.web.app/")
 public class ExpenditureService {
 
     @Autowired
