3048e41327734fc5bc5e40f458102ec2bfaaa989
Pradeepdeepu777 <kogatamk.19.beis@acharya.ac.in>
2024-08-31 11:50:23 +0530

    Update ExpenditureService.java

--- src/main/java/com/ganeshchaturthi/expenditure/ExpenditureService.java
+++ src/main/java/com/ganeshchaturthi/expenditure/ExpenditureService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Service
-@CrossOrigin(origins = "https://vinakayaexpenditure.web.app/")
+@CrossOrigin(origins = "https://frontend-n5mt.onrender.com/")
 public class ExpenditureService {
 
     @Autowired
